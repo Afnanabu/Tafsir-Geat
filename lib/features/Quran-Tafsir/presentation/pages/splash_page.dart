70e73d6e5a0adefc1da3ed40156efd56197263b5
Afnanabu <89046363+Afnanabu@users.noreply.github.com>
2021-10-17 13:53:06 +0300

    Add files via upload

---
+++ lib/features/Quran-Tafsir/presentation/pages/splash_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:greattafsire/features/Quran-Tafsir/presentation/pages/sura_page.dart';
+
+
+class SplashScreen extends StatefulWidget {
+
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    navigateToHomePage();
+  }
+
+  navigateToHomePage()async{
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context)=>AyatPage(),),);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assest/Page-Image/Image-Splach.png',),
+              fit: BoxFit.fill
+          ),
+        ),
+      ),
+    );
+  }
+
+}
